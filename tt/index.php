ea4556c6fc95397de34d9ff92ba28fc17ca81bbc
sheiciely <sheiciely_matias@hotmail.com>
2016-07-11 14:29:23 -0300

    novo projeto

---
+++ tt/index.php
@@ -0,0 +1,52 @@
+<!--
+||||||||||||||||||||||||||||||||||||||||||
+
+Web oficial: www.keyquotes.es
+Desarrollador: Álvaro Castillo
+Fecha: 1/4/2015
+
+Tutorial youtube. Efecto scroll en
+ menu con html, css y jQuery
+
+||||||||||||||||||||||||||||||||||||||||||
+-->
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>efecto scroll en menu</title>
+
+	<link rel="stylesheet" href="style/index_style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src='js/script.js'></script>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<a href='http://www.keyquotes.es'><li>Inicio</li></a>
+				<a href='http://www.keyquotes.es'><li>Videos</li></a>
+				<a href='http://www.keyquotes.es'><li>Im&aacute;genes</li></a>
+			</ul>
+				
+				<img id='logo' src='pictures/logo.png'>
+
+			<ul>
+				<a href='http://www.keyquotes.es'><li>Youtube</li></a>
+				<a href='http://www.keyquotes.es'><li>Nosotros</li></a>
+				<a href='http://www.keyquotes.es'><li>Contacto</li></a>
+			</ul>
+		</nav>
+	</header>
+
+	<section id='banner'>
+		
+	</section>
+
+	<section id='body'></section>
+</body>
+</html>
